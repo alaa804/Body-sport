73502cb59d01909c0d4d4804f1d61e5caa7abf16
OMAR-GODDI <73104649+OMAR-GODDI@users.noreply.github.com>
2021-01-12 15:58:08 +0100

    add

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -15,43 +15,6 @@ function NavBar() {
 
     return (
 
-        <header className="navbar">
-        
-        <img src={Logo} alt="Logo"/>
-        
-        <nav>
-        <ul className="navMenu">
-            <li>HOME</li>
-            <li>SHOP</li>
-            <li><Link to="/about">ABOUT</Link></li>
-            <li>MEMBERS</li>
-            <li><Link to="/contact">CONTACT </Link> </li> 
-        </ul>
-        </nav>
-
-
-  
-
-
-  
-  
-
-    
-
-
-
-        <div className="signeUp">
-            <ul className="menu">
-                <li>
-                <i className="Login fas fa-user-circle">Log in </i>
-                </li>
-            </ul>
-            <button className="shop">
-            <i className="shopping fas fa-shopping-basket">0</i>
-            </button>
-        </div>
-        </header>
-
         <React.Fragment>
         {isAuth ? <AuthLinks/> : <GuestLinks/> }
         </React.Fragment>
